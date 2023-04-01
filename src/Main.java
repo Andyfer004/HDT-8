20bbdeda29b893c365b085db2947e7296c27828b
GabrielPaz <paz221087@uvg.edu.gt>
2023-03-31 19:03:38 -0600

    Commit inicial GP

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,13 @@
+/*
+
+UNIVERSIDAD DEL VALLE DE GUATEMALA
+ALRGORITMOS Y ESTRUCTURAS DE DATOS
+Descripción: Hoja de trabajo 8 ADT Priority Queue
+Autor: GABRIEL PAZ & ANDY FUENTES
+Fecha: 29/03/2023
+*/
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
