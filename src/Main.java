bb6f8f1b3321685215369094dd83764492240489
Andyfer004 <99309349+Andyfer004@users.noreply.github.com>
2023-04-03 23:01:43 -0600

    javadoc comments

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
-/*
-
+/**
 UNIVERSIDAD DEL VALLE DE GUATEMALA
 ALRGORITMOS Y ESTRUCTURAS DE DATOS
-Descripción: Hoja de trabajo 8 ADT Priority Queue
+Descripcion: Hoja de trabajo 8 ADT Priority Queue
 Autor: GABRIEL PAZ & ANDY FUENTES
 Fecha: 29/03/2023
 */
